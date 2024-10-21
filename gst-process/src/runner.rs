611f2a91470104f08ad01fa030d5ddb73e316237
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-10-21 18:23:19 +0900

    gst-process remove comments

--- gst-process/src/runner.rs
+++ gst-process/src/runner.rs
@@ -8,26 +8,14 @@
  * Email: contact@nota.ai
  */
 
-use std::{env, path::PathBuf, process::Stdio};
+use std::{path::PathBuf, process::Stdio};
 
-// use gstreamer::prelude::{ElementExt, GstBinExtManual, GstObjectExt};
 use tokio::{
     io::{AsyncBufReadExt, BufReader},
     process::Command,
 };
 
 use crate::{project_root_path, utils::resources_path};
-
-// macro_rules! gst_make {
-//     ($call:expr, $invoker:expr) => {
-//         match $call {
-//             Ok(element) => element,
-//             Err(error) => {
-//                 return Err(format!("[{}] {}", $invoker, error.to_string()));
-//             }
-//         }
-//     };
-// }
 
 fn gstreamer_binary_path() -> PathBuf {
     PathBuf::from_iter([project_root_path!(), "assets\\gstreamer"])
@@ -63,7 +51,6 @@ fn gstreamer_plugin_scanner_path() -> PathBuf {
 
 fn location() -> String {
     String::from("location=rtsp://210.99.70.120:1935/live/cctv001.stream")
-    // String::from("rtsp://210.99.70.120:1935/live/cctv001.stream")
 }
 
 fn playlist_location() -> String {
@@ -72,7 +59,6 @@ fn playlist_location() -> String {
         resources_path().to_string_lossy()
     )
     .replace("\\", "\\\\")
-    // format!("{}\\output.m3u8", resources_path().to_string_lossy()).replace("\\", "\\\\")
 }
 
 fn segment_location() -> String {
@@ -81,7 +67,6 @@ fn segment_location() -> String {
         resources_path().to_string_lossy()
     )
     .replace("\\", "\\\\")
-    // format!("{}\\segment-%05d.ts", resources_path().to_string_lossy()).replace("\\", "\\\\")
 }
 
 pub async fn run() -> Result<(), String> {
@@ -111,98 +96,6 @@ pub async fn run() -> Result<(), String> {
         gstreamer_plugin_path.to_string_lossy(),
         gstreamer_plugin_scanner_path.to_string_lossy()
     );
-    // env::set_var("DYLD_LIBRARY_PATH", gstreamer_lib_path);
-    // env::set_var("GST_PLUGIN_PATH", gstreamer_plugin_path);
-    // env::set_var("GST_PLUGIN_SCANNER", gstreamer_plugin_scanner_path);
-    // env::set_var("PATH", gstreamer_binary_path);
-
-    // if let Err(error) = gstreamer::init() {
-    //     return Err(format!("[gstreamer::init] {}", error.to_string()));
-    // }
-
-    // let pipeline = gstreamer::Pipeline::new();
-    // let rtsprc = gst_make!(
-    //     gstreamer::ElementFactory::make("rtspsrc")
-    //         .property("location", &location)
-    //         .property("latency", &100i32)
-    //         .build(),
-    //     "rtsprc"
-    // );
-    // let rtsph264depay = gst_make!(
-    //     gstreamer::ElementFactory::make("rtsph264depay").build(),
-    //     "rtsph264depay"
-    // );
-    // let h264parse = gst_make!(
-    //     gstreamer::ElementFactory::make("h264parse").build(),
-    //     "h264parse"
-    // );
-    // let mpegtsmux = gst_make!(
-    //     gstreamer::ElementFactory::make("mpegtsmux").build(),
-    //     "mpegtsmux"
-    // );
-    // let hlssink = gst_make!(
-    //     gstreamer::ElementFactory::make("hlssink")
-    //         .property("max-files", &100u32)
-    //         .property("target-duration0", &1u32)
-    //         .property("playlist-length", &100u32)
-    //         .property("playlist-location", &playlist_location)
-    //         .property("location", &segment_location)
-    //         .build(),
-    //     "hlssink"
-    // );
-
-    // if let Err(error) =
-    //     pipeline.add_many(&[&rtsprc, &rtsph264depay, &h264parse, &mpegtsmux, &hlssink])
-    // {
-    //     return Err(format!("[pipeline.add_many] {}", error.to_string()));
-    // }
-
-    // if let Err(error) =
-    //     gstreamer::Element::link_many(&[&rtsph264depay, &h264parse, &mpegtsmux, &hlssink])
-    // {
-    //     return Err(format!(
-    //         "[gstreamer::Element::link_many] {}",
-    //         error.to_string()
-    //     ));
-    // }
-
-    // match pipeline.set_state(gstreamer::State::Playing) {
-    //     Ok(status) => {
-    //         if status == gstreamer::StateChangeSuccess::Success {
-    //             if let Some(bus) = pipeline.bus() {
-    //                 for message in bus.iter_timed(gstreamer::ClockTime::NONE) {
-    //                     use gstreamer::MessageView;
-
-    //                     match message.view() {
-    //                         MessageView::Eos(..) => {
-    //                             println!("End of stream");
-    //                             break;
-    //                         }
-    //                         MessageView::Error(err) => {
-    //                             eprintln!(
-    //                                 "Error received from element {:?}: {}",
-    //                                 err.src().map(|s| s.path_string()),
-    //                                 err.error()
-    //                             );
-    //                             break;
-    //                         }
-    //                         _ => (),
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     Err(error) => {
-    //         return Err(format!(
-    //             "[pipeline.set_state(Playing)] {}",
-    //             error.to_string()
-    //         ))
-    //     }
-    // };
-
-    // if let Err(error) = pipeline.set_state(gstreamer::State::Null) {
-    //     return Err(format!("[pipeline.set_state(Null)] {}", error.to_string()));
-    // }
 
     match Command::new(gstreamer_launch_path)
         .args(&[
